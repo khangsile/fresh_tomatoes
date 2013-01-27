c6a5fd8a66a707c07cf6708ceea0b48bcff38b3f
khangsile <khangsile@gmail.com>
2013-01-27 17:46:29 -0500

    formatted output

--- js/lookup.js
+++ js/lookup.js
@@ -1,7 +1,8 @@
 function lookup(query) {
     var API_KEY = "qbqf3mzakee76b8xn7qwhw8q";
+    var url = "www.rottentomatoes.com/m/"
     var req = new XMLHttpRequest();
-    
+   
     console.log("Made it here");
     req.open("GET", 
 	     "http://api.rottentomatoes.com/api/public/v1.0/movies.json?" + 
@@ -14,24 +15,61 @@ function lookup(query) {
 	if (req.status == 200  & req.readyState == 4) {
 	    var resp = req.responseText;
 	    var obj = jQuery.parseJSON(resp);
+	    
 	    var movie = obj.movies[0];
 	    var ratings = movie.ratings;
-	    console.log("How about here");
+	    var crating = 'Not Rated', cscore = '??';
+
+	    if (ratings.critics_rating != null)
+		crating = ratings.critics_rating;
+	    if (ratings.critics_score >= 0)
+		cscore = ratings.critics_score;
 	    
+
 	    $(document.body).append("<div id='movie'>" + 
-				    movie.title + "-------------------------" +
-				    ratings.critics_rating + "-" + 
-				    ratings.critics_score + 
+				    "<a href = '#' onclick='seePage()' " +
+				    "name='" + 
+				    movie.title.replace(/ /g, '_')
+				    +  "'>" +
+				    movie.title + "</a>" + 
+				    " ("+
+				    crating + " - " + 
+				    cscore + ")" +
 				    "</div>");
+	    setClick(movie.title.replace(/ /g, '_'));
 	}
     }
+    
 
     req.send();
 }
 
+function setClick(movie) {
+    $(document).ready(function() {
+	    $('a[name='+ movie + ']').click(function() {
+		    movie = movie.replace(/:/g, '').replace(/-/g, '');
+		    var url = "http://www.rottentomatoes.com/m/" + movie + "/";
+		    chrome.tabs.create({url: url});
+		});
+	});
+}
+
+
 $( document ).ready(function() {
-	$( '#searchSub' ).click(function() {
-		q = $( '#query' ).val();
-		lookup(q);
+	$( '#searchsub' ).click(function() {
+		getQuery();
 	    });
-    });
+	$(function() {
+		$('#query').keypress(function (e) {
+			if (e.which==13 || e.keyCode==13) {
+			    getQuery();
+			}
+		    });
+	    });
+});
+
+function getQuery() {
+    q = $( '#query' ).val();
+    $( '#query' ).val('');
+    lookup(q);
+}
